b1b9e8cfad5088ccc09ae8e38e61144c3cfde052
KingOKarma <virtualdisaster782@gmail.com>
2021-05-01 03:15:25 +0100

    events for subs, cheers, raids, follows

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # TwitchWebhook
 This is a test mainly for practicing how webhooks work and how twitch webhooks work/ Twitch-EventSub
+
+---
+### Setup
+
+- run in your terminal  `npm install` or if you're using yarn `yarn`
+
+- Rename the file `example.config.yml` to `config.yml`
+
+- Write your parameters into the file, notes are written in the file:
+```yml
+clientID: Your Client ID
+# When getting your client ID 
+# Make sure the broadcaster you are getting the client ID for has authorised this link
+
+#https://id.twitch.tv/oauth2/authorize?client_id=<CLIENTID>&redirect_uri=<OneOfTheLinksFromYourRedirectURL>&response_type=token&scope=bits:read%20channel:read:subscriptions
+# <CLIENTID> = Your Client ID
+# <OneOfTheLinksFromYourRedirectURL> = This is one of your links from the "OAuth Redirect URLs" on your twitch dev console
+
+#Twitch Dev console can be found at:
+#https://dev.twitch.tv/console/
+clientSecret: This is your client secret you can obtain it from the offical twitch dev dashboard
+
+```
+
+### Runing the script
+
+In your terminal just run `npm start` or `yarn start`
+
+if you want to just use node, from the folder root run `node build/main.js`
--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ const { clientSecret } = CONFIG;
 const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
 const apiClient = new ApiClient({ authProvider });
 
+void apiClient.helix.eventSub.deleteAllSubscriptions();
+
 const listener = new EventSubListener(apiClient, new NgrokAdapter(), "ciW$k&8Q4mue3neEPQ4Q&5mV5p!LpsHw7u55ZaH#X8vBP&YZqMLX%NE45Rph");
 listener.listen().catch(console.error);
 
@@ -21,12 +23,60 @@ const userId = "192135643";
 
 async function initTwitch(): Promise<void> {
 
-    await listener.subscribeToChannelUpdateEvents(userId, (e) => {
-        console.log(`${e.broadcasterDisplayName} Has set the new stream title to: "${e.streamTitle}"`);
-    }).catch(console.error);
+    await listener.subscribeToChannelUpdateEvents(userId, async (channel) => {
+        console.log(`${channel.broadcasterDisplayName} Has set the new stream title to: "${channel.streamTitle}"`);
+    });
 
-    await listener.subscribeToStreamOfflineEvents(userId, (e) => {
-        console.log(`${e.broadcasterDisplayName} just went offline`);
+    await listener.subscribeToStreamOfflineEvents(userId, (channel) => {
+        console.log(`${channel.broadcasterDisplayName} just went offline`);
+    });
+
+    await listener.subscribeToChannelFollowEvents(userId, (channel) => {
+        console.log(`${channel.userDisplayName} has just followed!`);
+    });
+
+    await listener.subscribeToChannelSubscriptionEvents(userId, (channel) => {
+
+        let subMessage;
+
+        switch (channel.tier) {
+            case "1000":
+                subMessage = `${channel.userDisplayName} Is now a tier 1 Sub!`;
+                break;
+
+
+            case "2000":
+                subMessage = `${channel.userDisplayName} Is now a tier 2 Sub!`;
+                break;
+
+
+            case "3000":
+                subMessage = `${channel.userDisplayName} Is now a tier 3 Sub!`;
+                break;
+        }
+
+        if (channel.isGift) {
+            subMessage.concat(" (This was a gift!)");
+        }
+
+        console.log(subMessage);
+    }
+    );
+
+    await listener.subscribeToChannelCheerEvents(userId, (channel) => {
+        switch (channel.isAnonymous) {
+            case true:
+                console.log(`An anonymous user has just cheered ${channel.bits} PogChamp \n\n ${channel.message}`);
+                break;
+
+            case false:
+                console.log(`${channel.userDisplayName} has just cheered ${channel.bits} PogChamp \n\n ${channel.message}`);
+                break;
+        }
+    });
+
+    await listener.subscribeToChannelRaidEventsTo(userId, (channel) => {
+        console.log(`${channel.raidingBroadcasterDisplayName} is raiding with ${channel.viewers} viewers!`);
     });
 
     console.log("All Notif requests have been initialised successfully");
