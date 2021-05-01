85fbc4d8177af1f86bf7c1be911a4afbcda58210
KingOKarma <virtualdisaster782@gmail.com>
2021-05-01 01:20:31 +0100

    testing part 2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { EventSubListener, ReverseProxyAdapter } from "twitch-eventsub";
 import { ApiClient } from "twitch";
 import { CONFIG } from "./utils/globals";
 import { ClientCredentialsAuthProvider } from "twitch-auth";
-// Import { NgrokAdapter } from "twitch-eventsub-ngrok";
+import { EventSubListener } from "twitch-eventsub";
+import { NgrokAdapter } from "twitch-eventsub-ngrok";
 
 
 const clientId = CONFIG.clientID;
@@ -11,11 +11,7 @@ const { clientSecret } = CONFIG;
 const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
 const apiClient = new ApiClient({ authProvider });
 
-const listener = new EventSubListener(apiClient, new ReverseProxyAdapter({
-    externalPort: 443, // The external port (optional, defaults to 443)
-    hostName: "api.bucketbot.dev" // The host name the server is available from
-
-}), "ciW$k&8Q4mue3neEPQ4Q&5mV5p!LpsHw7u55ZaH#X8vBP&YZqMLX%NE45Rph");
+const listener = new EventSubListener(apiClient, new NgrokAdapter(), "ciW$k&8Q4mue3neEPQ4Q&5mV5p!LpsHw7u55ZaH#X8vBP&YZqMLX%NE45Rph");
 listener.listen().catch(console.error);
 
 // Void apiClient.helix.eventSub.deleteAllSubscriptions();
