12758ef9636156494c90dbc05d48c1362d721e91
KingOKarma <virtualdisaster782@gmail.com>
2021-05-03 16:07:31 +0100

    testing changes

--- src/main.ts
+++ src/main.ts
@@ -11,15 +11,7 @@ const app = express();
 // Set port
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 const port = process.env.PORT !== null || 8080;
-
-app.use(express.static(`${__dirname}/public`));
-
 // Routes
-
-app.get("/", (req, res) => {
-    res.render("index");
-});
-
 app.listen(port, () => {
     console.log("App running");
 });
