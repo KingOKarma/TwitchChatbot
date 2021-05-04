32fe8bd932483519fba72b8fdab7ece0398bccf6
KingOKarma <virtualdisaster782@gmail.com>
2021-05-04 20:38:00 +0100

    added external port: 443

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ async function initTwitch(): Promise<void> {
     void await apiClient.helix.eventSub.deleteAllSubscriptions();
 
     const adapter = new ReverseProxyAdapter({
+        externalPort: 443,
         hostName: "twitch.bucketbot.dev",
         pathPrefix: "/events",
         port: 3000
